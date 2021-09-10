7a2718c630763a2554bf425927d74daf24c0a14a
franticus <70944846+franticus@users.noreply.github.com>
2021-09-10 18:45:32 +0300

    iphoneXR

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# tbanner
-tbanner task v
+#tBanner Verv
+Html / Scss/ Js / Gulp
+Link: tbanner-verv.netlify.app
+
+![Screenshot_140](https://user-images.githubusercontent.com/70944846/132877858-c8e3fc68-658a-491d-95fb-bece124366d6.png)
+
+Demo:
+https://user-images.githubusercontent.com/70944846/132880706-9e676e38-7cd2-46ae-9416-5de4f8f28a45.mp4
--- tbanner/js/script.js
+++ tbanner/js/script.js
@@ -3,13 +3,21 @@ const secondMonth = document.querySelector('#second');
 const thirdMonth = document.querySelector('#third');
 const continueBtn = document.querySelector('.continue')
 const pro = document.querySelector('.graph_pro')
+const cross = document.querySelector('.cross')
+const sliderItem = document.querySelectorAll('.slider_item')
+const swiperSlide = document.querySelectorAll('.swiper_slide')
+const monthItem = document.querySelectorAll('.month_item')
+const wrapper = document.querySelector('.wrapper_container')
+const copyright = document.querySelector('.copyright')
+
 
 const swiper = new Swiper(".mySwiper", {
     observer: true,
     observeParents: true,
     loop: true,
-    spaceBetween: 10,
+    // spaceBetween: 10,
     slidesPerView: 3,
+    initialSlide: 10,
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
@@ -24,7 +32,6 @@ const show = () => {
     }, 1400)
 }
 show()
-
 const resetChoose = () => {
     firstMonth.classList.remove('active')
     secondMonth.classList.remove('active')
@@ -37,7 +44,21 @@ const activePlan = (plan, link) => {
         continueBtn.setAttribute("href", link);
     })
 }
-
 activePlan(firstMonth, "https://www.google.com/search?q=1")
 activePlan(secondMonth, "https://www.google.com/search?q=2")
 activePlan(thirdMonth, 'https://www.google.com/search?q=3')
+
+
+cross.addEventListener('click', () => {
+    wrapper.classList.toggle('dark')
+    copyright.classList.toggle('dark')
+    sliderItem.forEach((item) => {
+        item.classList.toggle('dark')
+    })
+    monthItem.forEach((item) => {
+        item.classList.toggle('dark')
+    })
+    swiperSlide.forEach((item) => {
+        item.style.background = '#000';
+    })
+})
